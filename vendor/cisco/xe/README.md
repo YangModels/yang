bdee730098d87039b647d12d620df8869f364ee0
Pravin Gohite <pgohite@users.noreply.github.com>
2016-08-04 15:27:18 -0700

    Merge Cisco IOS XE 16.3.1 Release model content (#84)
    Initial IOS XE 16.3.1 yang models.

--- vendor/cisco/xe/README.md
+++ vendor/cisco/xe/README.md
@@ -1,1 +1,9 @@
-This directory will contain models specific to IOS-XE platforms.
+This directory contains OS/platform-specific YANG models for Cisco's IOS-XE platforms.
+
+The directory is _currently_ organized by OS-version, with each sub-directory containing the models for that version. The OS version number is presented as a single number. Thus the YANG models for IOS-XE 16.3.1 will be in the subdirectory named "1631". Please note that this organization may change.
+
+A README file may exist in the version subdirectories with any specific notes relating to the models in the directory.
+
+Further documentation on Cisco's IOS-XE YANG-based interfaces may be found at:
+
+* [Cisco IOS XE Data Models Configuration Guide](TBD)
---
+++ vendor/cisco/xe/check.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+#
+# Vendor-specific check script. Assumes that pyang is on path and that
+# all standard modules are on its internal module path.
+#
+# Deviation modules are NOT checked as they require specific imports
+# typically not available locally.
+#
+
+platform_dir="vendor/cisco/xe"
+to_check="1631 1631/MIBS"
+pyang_flags="-p 1631:1631/MIBS:../../../standard/ietf/RFC"
+
+checkDir () {
+    echo Checking yang files in $platform_dir/$1
+    exit_status=""
+    for f in $1/*.yang; do
+    	errors=`pyang $pyang_flags $f 2>&1 | grep "error:"`
+    	if [ ! -z "$errors" ]; then
+    	    echo Errors in $f
+    	    exit_status="failed!"
+        fi
+    done
+    
+    if [ ! -z "$exit_status" ]; then
+	   exit 1
+    fi
+}
+
+echo Checking modules with pyang command:
+printf "\n    pyang $pyang_flags MODULE\n\n"
+
+if [ -e "$$platform_dir" ]; then
+    cd $platform_dir
+fi
+
+for d in $to_check; do
+    checkDir $d
+done
