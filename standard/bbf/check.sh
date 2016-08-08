417aba1bf4f6e952ff3eceabeef4053098a4f7d8
William Lupton <wfl@cantab.net>
2016-08-08 12:09:05 +0100

    Fix check.sh script (was assuming wrong current directory)

--- standard/bbf/check.sh
+++ standard/bbf/check.sh
@@ -7,15 +7,17 @@
 # * https://github.com/mbj4668/pyang/issues/232
 # * https://github.com/mbj4668/pyang/commit/77fdee4
 
+ietf_dir="standard/ietf"
+bbf_dir="standard/bbf"
 to_check="common interface"
 
 cwd=`pwd`
-ietf=$cwd/../ietf
 
-pyang_flags="--strict --max-line-length=70 --lint --lint-modulename-prefix=bbf --lint-namespace-prefix=urn:bbf:yang: --verbose --path=$cwd --path=$ietf"
+pyang_flags="--strict --max-line-length=70 --lint --lint-modulename-prefix=bbf --lint-namespace-prefix=urn:bbf:yang: --verbose --path=$cwd/$ietf_dir --path=$cwd/$bbf_dir"
 
 setupPyang () {
     local cwd=`pwd`
+    cd $bbf_dir
     if [ -d pyang-temp ]; then
         (cd pyang-temp; git checkout master; git pull)
     else
@@ -28,10 +30,11 @@ setupPyang () {
 }
 
 checkDir () {
-    echo Checking yang files in $1
+    local dir="$bbf_dir/$1"
+    echo Checking yang files in $dir
     exit_status=""
     printf "\n"
-    for f in `find $1 -name '*.yang'`; do
+    for f in `find $dir -name '*.yang'`; do
         printf "pyang $pyang_flags $f\n"
 	errors=`pyang $pyang_flags $f 2>&1 | grep "error:"`
 	if [ ! -z "$errors" ]; then
