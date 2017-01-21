0513fb0c449fd82a6fb86445f95a2cc96e19e550
prgohite <prgohite@cisco.com>
2017-01-20 16:07:01 -0800

    Added Cisco IOS XE release 16.4.1 yang models

--- vendor/cisco/xe/check.sh
+++ vendor/cisco/xe/check.sh
@@ -8,19 +8,25 @@
 #
 
 platform_dir="vendor/cisco/xe"
-to_check="1631 1631/MIBS 1632 1632/MIBS"
+to_check="1631 1631/MIBS 1632 1632/MIBS 1641 1641/MIBS"
 inc_path="../../../../standard/ietf/RFC"
-pyang_flags="..:$inc_path"
+pyang_flags=".:$inc_path"
 
 checkDir () {
     echo Checking yang files in $platform_dir/$1
     cwd=`pwd`
     cd $1
     exit_status=""
+    pyang_flags=".:$inc_path"
+    if [ `basename $PWD` == "MIBS" ]; then
+        pyang_flags="..:../$inc_path"
+    fi
+
     for f in *.yang; do
         errors=`pyang -p $pyang_flags $f 2>&1 | grep "error:"`
     	if [ ! -z "$errors" ]; then
     	    echo Errors in $f
+            echo $errors
     	    exit_status="failed!"
         fi
     done
