74db78c936f2b1e6cd4ba9513742670a5840a0ea
MarcHolness <mholness@ciena.com>
2017-09-20 14:30:03 -0400

    Qcp version 1.2 changes

---
+++ standard/ieee/check.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+#
+# Possible IEEE check script. Assumes that pyang is on path and that
+# all standard modules are on its internal module path.
+#
+ieee_dir="standard/ieee"
+to_check="802.1"
+
+# relax constraint for now
+# add --ietf if you want to do strict IETF checking
+pyang_flags="--verbose"
+
+checkDir () {
+    local dir="$ieee_dir/$1"
+    echo Checking yang files in $dir
+    exit_status=""
+    cwd=`pwd`
+    cd $dir
+    printf "\n"
+    for f in *.yang; do
+        printf "pyang $pyang_flags $f\n"
+	errors=`pyang $pyang_flags $f 2>&1 | grep "error:"`
+	if [ ! -z "$errors" ]; then
+	    echo Errors in $f
+            printf "\n  $errors \n"
+	    exit_status="failed!"
+	fi
+    done
+    if [ ! -z "$exit_status" ]; then
+	exit 1
+    fi
+    cd $cwd
+}
+
+echo Checking modules with pyang command:
+printf "\n    pyang $pyang_flags MODULE\n\n"
+
+for d in $to_check; do
+    checkDir $d
+done
