3e3b5e198255c5cc5c974a9bc5fe7fa91c371ee9
Joe Clarke <jclarke@cisco.com>
2017-10-05 15:10:00 -0400

    Make sure we have basic module parameters, even if a search error occurred.

--- tools/api/yangSearch/module.py
+++ tools/api/yangSearch/module.py
@@ -27,33 +27,33 @@ import urllib
 
 class Module(object):
     __object_dict = {
-        'name' : true,
-        'revision' : true,
-        'organization' : true,
-        'ietf' : true,
-        'namespace' : true,
-        'schema' : true,
-        'generated-from' : true,
-        'maturity-level' : true,
-        'document-name' : true,
-        'author-email' : true,
-        'reference' : true,
-        'module-classification' : true,
-        'compilation-status' : true,
-        'compilation-result' : true,
-        'prefix' : true,
-        'yang-version' : true,
-        'description' : true,
-        'contact' : true,
-        'module-type' : true,
-        'belongs-to' : true,
-        'tree-type' : true,
-        'submodule' : true,
-        'dependencies' : false,
-        'dependents' : false,
-        'semantic-version' : true,
-        'derived-semantic-version' : true,
-        'implementations' : true
+        'name': true,
+        'revision': true,
+        'organization': true,
+        'ietf': true,
+        'namespace': true,
+        'schema': true,
+        'generated-from': true,
+        'maturity-level': true,
+        'document-name': true,
+        'author-email': true,
+        'reference': true,
+        'module-classification': true,
+        'compilation-status': true,
+        'compilation-result': true,
+        'prefix': true,
+        'yang-version': true,
+        'description': true,
+        'contact': true,
+        'module-type': true,
+        'belongs-to': true,
+        'tree-type': true,
+        'submodule': true,
+        'dependencies': false,
+        'dependents': false,
+        'semantic-version': true,
+        'derived-semantic-version': true,
+        'implementations': true
     }
 
     __seen_modules = {}
@@ -144,6 +144,14 @@ class Module(object):
         arr = {}
         for key, value in Module.__object_dict.items():
             arr[key] = self.__dict[key]
+
+        # We do this in case there was an error searching for the module.
+        if 'name' not in arr:
+            arr['name'] = self.__name
+        if 'revision' not in arr:
+            arr['revision'] = self.__revision
+        if 'organization' not in arr:
+            arr['organization'] = self.__organization
 
         return arr
 
