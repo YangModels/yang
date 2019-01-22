6473a8e7dba122f6d992e9405759a2a0888931fb
Michael Vorburger <vorburger@redhat.com>
2019-01-22 22:20:15 +0100

    minor fix to sync.sh

--- sync.sh
+++ sync.sh
@@ -19,6 +19,7 @@ git submodule foreach git pull --ff-only origin master
 popd
 
 git rm experimental/odp/*
+mkdir experimental/odp
 
 # There is a probably a better way to do this, but this works... ;-)
 find /home/vorburger/dev/ODL/git/releng/autorelease -name "*.yang" -path "*src/main/yang*" ! -path "*test*" -printf 'cp %p experimental/odp/%f\n' >/tmp/copy-yang.sh
