703e50496c3d45e25ff7aa8d3dce20755758d02a
Michael Vorburger <vorburger@redhat.com>
2019-01-22 22:11:58 +0100

    add script to sync from OpenDaylight (#556)

---
+++ sync.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# TODO create sub-directories per opendaylight branch
+
+if [ -z "$1" ]; then
+  echo "USAGE: sync.sh <PATH-TO-YOUR-opendaylight.git/releng/autorelease>"
+  exit
+fi
+set -ex
+
+pushd .
+cd $1
+git pull
+git submodule init
+git submodule update
+git submodule foreach git reset --hard
+git submodule foreach git checkout origin/master
+git submodule foreach git pull --ff-only origin master
+popd
+
+git rm experimental/odp/*
+
+# There is a probably a better way to do this, but this works... ;-)
+find /home/vorburger/dev/ODL/git/releng/autorelease -name "*.yang" -path "*src/main/yang*" ! -path "*test*" -printf 'cp %p experimental/odp/%f\n' >/tmp/copy-yang.sh
+source /tmp/copy-yang.sh
+
+git add experimental/odp/*
