9919c8aeb55e28f4737ebe78202e16bd46dae972
Kyoung-Hwan Yun <kyyun@cisco.com>
2019-07-21 22:48:03 -0700

    added models for 9.3-1

--- vendor/cisco/nx/check.sh
+++ vendor/cisco/nx/check.sh
@@ -11,7 +11,7 @@
 # be removed.
 #
 platform_dir="vendor/cisco/nx"
-to_check="7.0-3-I7-5 7.0-3-I7-6 9.2-3"
+to_check="7.0-3-I7-5 7.0-3-I7-6 9.3-1"
 inc_path="."
 pyang_flags="--lax-quote-checks"
 debug="0"
