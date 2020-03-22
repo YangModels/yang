03e9afa78062fbb635cc57cb6862657728e620b2
Alfred F Leung <alfleung@cisco.com>
2020-03-21 17:58:32 -0700

    IOS-XR 7.0.2 models

--- vendor/cisco/xr/check.sh
+++ vendor/cisco/xr/check.sh
@@ -11,7 +11,7 @@
 # should be removed.
 #
 platform_dir="vendor/cisco/xr"
-to_check="623 632 642 651 652 653 662 663 701 711"
+to_check="623 632 642 651 652 653 662 663 701 702 711"
 debug=0
 checkDir () {
     if [ "$debug" -eq "1" ]; then
