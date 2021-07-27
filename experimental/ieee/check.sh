c34eb9f1e4775ad22172dbfc615097d6dae284d5
Slavomir Mazur <slavomir.mazur@pantheon.tech>
2021-07-27 10:53:20 +0200

    Introduction of Github actions
    - check.yml configuration file added
    - ieee and ietf check.sh scripts format and output more unified
    
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- experimental/ieee/check.sh
+++ experimental/ieee/check.sh
@@ -3,45 +3,44 @@
 # check script. Assumes that pyang is on path and that
 # all standard modules are on its internal module path.
 #
-base_dir="`pwd`/experimental/ieee/"
+cwd=$(pwd)
+ietf_dir="standard/ietf"
+ieee_dir="standard/ieee"
+ieee_experimental_dir="experimental/ieee"
 #to_check="802.1 802.3 1588"
 to_check="802.1 1588 1906.1"
 
 # relax constraint for now
 # add --ietf if you want to do strict IETF checking
-pyang_flags="--verbose -p ../../../standard/ietf/RFC/ -p ../../../standard/ieee/published/802 -p ../../../standard/ieee/published/802.1/ -p ../../../standard/ieee/published/802.3/ -p ../../../experimental/ieee/1588/ -p ../../../experimental/ieee/1588/ -p ../../../standard/ieee/draft/802.1/Qcw -p ../../../standard/ieee/draft/802.1/Qcr "
+ietf_dir_flag="--path $cwd/$ietf_dir/RFC/"
+pyang_flags="--verbose --path $ietf_dir_flag --path $cwd/$ieee_dir/published/802 --path $cwd/$ieee_dir/published/802.1/ --path $cwd/$ieee_dir/published/802.3/ --path $cwd/$ieee_experimental_dir/1588/ --path $cwd/$ieee_experimental_dir/1588/ --path $cwd/$ieee_dir/draft/802.1/Qcw --path $cwd/$ieee_dir/draft/802.1/Qcr "
 
-checkDir () {
-    echo Checking yang files in $1
+checkDir() {
+    local dir="$ieee_experimental_dir/$1"
+    echo "\nChecking yang files in $dir"
     exit_status=""
-    cwd=`pwd`
-    cd "$base_dir/$1"
+    cd $dir
     printf "\n"
     for f in *.yang; do
         printf "pyang $pyang_flags $f\n"
-#        `pyang $pyang_flags $f`
-	errors=`pyang $pyang_flags $f 2>&1 | grep "error:"`
-	if [ ! -z "$errors" ]; then
-	    echo Errors in $f
+        errors=$(pyang $pyang_flags $f 2>&1 | grep "error:")
+        if [ ! -z "$errors" ]; then
+            echo Errors in $f
             printf "\n  $errors \n"
-	    exit_status="failed!"
-	fi
+            exit_status="failed!"
+        fi
     done
     if [ ! -z "$exit_status" ]; then
-	exit 1
+        exit 1
     fi
     cd $cwd
 }
 
-
 # check modules in IEEE 802.X subdirectories
-printf "\n Checking IEEE modules in $base_dir \n" 
-
-echo cd "./$base_dir"
+printf "\nChecking IEEE experimental modules in $cwd/$ieee_experimental_dir\n"
+echo Checking modules with pyang command:
+printf "\n    pyang $pyang_flags MODULE\n"
 
 for d in $to_check; do
-    printf "\n Checking modules with pyang in $to_check : \n"
     checkDir $d
 done
-
-
--- standard/ieee/check.sh
+++ standard/ieee/check.sh
@@ -4,43 +4,43 @@
 # all standard modules are on its internal module path.
 # Also assumes that the script is run from the root of the yang master.
 #
+cwd=$(pwd)
+ietf_dir="standard/ietf"
 ieee_dir="standard/ieee"
 to_check="draft/802 draft/802.1 draft/802.1/ABcu draft/802.1/CBcv draft/802.1/CBdb draft/802.1/Qcw draft/802.1/QRev draft/802.1/Qcz draft/802.3 published/1906.1 published/802 published/802.1"
 
-checkDir () 
-{
+# relax constraint for now
+# add --ietf if you want to do strict IETF checking
+ietf_dir_flag="--path $cwd/$ietf_dir/RFC/"
+
+checkDir() {
     local dir="$ieee_dir/$1"
-
-    echo Checking yang files in $dir
+    echo "\nChecking yang files in $dir"
     exit_status=""
-    pyang_flags="--verbose -p . -p $cwd/$ieee_dir/../ietf/RFC/ -p $cwd/$ieee_dir/published/802 -p $cwd/$ieee_dir/published/802.1"
-
+    pyang_flags="--verbose --path $ietf_dir_flag --path $cwd/$ieee_dir/published/802 --path $cwd/$ieee_dir/published/802.1"
     cd $dir
 
     if [ -f "./check_pyang_extra_flags" ]; then
-        check_pyang_extra_flags=`cat ./check_pyang_extra_flags`
+        check_pyang_extra_flags=$(cat ./check_pyang_extra_flags)
         pyang_flags="$pyang_flags $check_pyang_extra_flags"
     fi
     printf "\n"
     for f in *.yang; do
         printf "pyang $pyang_flags $f\n"
-#        `pyang $pyang_flags $f`
-        errors=`pyang $pyang_flags $f 2>&1 | grep "error:"`
+        errors=$(pyang $pyang_flags $f 2>&1 | grep "error:")
         if [ ! -z "$errors" ]; then
-        	echo Errors in $f
-        	printf "\n  $errors \n"
-        	exit_status="failed!"
+            echo Errors in $f
+            printf "\n  $errors \n"
+            exit_status="failed!"
         fi
     done
     if [ ! -z "$exit_status" ]; then
-    	exit 1
+        exit 1
     fi
     cd $cwd
 }
 
 echo Checking modules with pyang command:
-cwd=`pwd`
 for d in $to_check; do
-    printf "\n Checking modules with pyang in $d : \n"
     checkDir $d
 done
