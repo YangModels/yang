2ffda6532b4b8d79f45bd2250a6d04b992d989e5
SlavomirMazurPantheon <67378888+SlavomirMazurPantheon@users.noreply.github.com>
2021-08-07 18:12:01 +0200

    Introduction of Github actions (#1053)
    - check.yml configuration file added
    - ieee and ietf check.sh scripts format and output more unified
    
    Signed-off-by: Slavomir Mazur <slavomir.mazur@pantheon.tech>

--- standard/ietf/check.sh
+++ standard/ietf/check.sh
@@ -3,7 +3,7 @@
 # Possible IETF check script. Assumes that pyang is on path and that
 # all standard modules are on its internal module path.
 #
-cwd=`pwd`
+cwd=$(pwd)
 ietf_dir="standard/ietf"
 ieee_dir="standard/ieee"
 to_check="RFC"
@@ -13,29 +13,29 @@ to_check="RFC"
 ieee_dir_flag="--path $cwd/$ieee_dir/published/"
 pyang_flags="--verbose $ieee_dir_flag"
 
-checkDir () {
+checkDir() {
     local dir="$ietf_dir/$1"
-    echo Checking yang files in $dir
+    echo "\nChecking yang files in $dir"
     exit_status=""
     cd $dir
     printf "\n"
     for f in *.yang; do
         printf "pyang $pyang_flags $f\n"
-	errors=`pyang $pyang_flags $f 2>&1 | grep "error:"`
-	if [ ! -z "$errors" ]; then
-	    echo Errors in $f
+        errors=$(pyang $pyang_flags $f 2>&1 | grep "error:")
+        if [ ! -z "$errors" ]; then
+            echo Errors in $f
             printf "\n  $errors \n"
-	    exit_status="failed!"
-	fi
+            exit_status="failed!"
+        fi
     done
     if [ ! -z "$exit_status" ]; then
-	exit 1
+        exit 1
     fi
     cd $cwd
 }
 
 echo Checking modules with pyang command:
-printf "\n    pyang $pyang_flags MODULE\n\n"
+printf "\n    pyang $pyang_flags MODULE\n"
 
 for d in $to_check; do
     checkDir $d
